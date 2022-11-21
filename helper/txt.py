b27c6912ed00fbc9b13a15f09434055157c66500
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-11-21 09:50:40 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -1,7 +1,7 @@
 """
 Apache License 2.0
 
-Copyright (c) 2022 @PYRO_BOTZ
+Copyright (c) 2022 @LazyDeveloper
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
